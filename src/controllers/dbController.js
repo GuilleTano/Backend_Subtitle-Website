e04d688dc6b6708fce703ce94dc6e8be7afcc201
Guillermo Taño <guillermotano@hotmail.com>
2026-05-17 18:18:29 -0300

    Servidor y conexion a mongoDB

---
+++ src/controllers/dbController.js
@@ -0,0 +1,67 @@
+import {connectDB} from "../mongDBConnection/dbConnection.js";
+const controller = {};
+
+
+// Get para obtener subs (por ahora datos en general)
+controller.getSubs = async (req, res) => {
+    try {
+
+        const subDB = await connectDB();
+        //const collection = subDB.collection("NOMBRE DE COLECCION");
+
+        const collection = subDB.collection("movies_metadata");
+
+        const query = {
+            original_title: "Toy Story"
+        };
+
+        const options = {
+            projection: {
+                original_title: 1
+            }
+        };
+
+        const result = await collection.findOne(query, options);
+
+        //const result = await collection.find(query, options); 
+        // find() devuelve un cursor, no un array. Habrá que usar .toArray()
+
+        res.json(result);
+
+    } catch (err){
+        console.error(err);
+        res.status(500).json({ error: 'No se pudo procesar la solicitud' });
+    }
+}
+
+
+// Post para agregar subs  HACERLO ASI O CON SCRIPT INTERNO?
+controller.addSubs = async (req, res) => {
+    try{
+
+        // Recibe el archivo y separa su contenido
+        const {series, episode, filename, content} =req.body;
+        
+        // Crea un objeto con la metadata y el contenido de subtitulos
+        const newSub = {
+            series: series,
+            episode: episode,
+            filename: filename,
+            content: content
+        };
+
+        // Conexion a DB
+        const subDB = await connectDB();
+        //const collection = subDB.collection("NOMBRE DE COLECCION");
+
+        // AQUI DEBERIA CREARSE EL DOCUMENTO Y ENVIARSE A MONGO
+
+    } catch(err) {
+        console.error(err);
+        res.status(500).json({ error: 'No se pudo procesar la solicitud' });
+    }
+}
+
+
+
+export {controller}
