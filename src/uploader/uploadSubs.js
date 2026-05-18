fd7afcbebe0f95a7badb99a823e4b974bf7bba3f
Guillermo Taño <guillermotano@hotmail.com>
2026-05-18 17:28:25 -0300

    Upload obj finished

--- src/uploader/uploadSubs.js
+++ src/uploader/uploadSubs.js
@@ -5,23 +5,28 @@ const uploads = "./uploads";
 const fileList = await fs.readdir(uploads); // Lee el directorio y devuelve un array con los nombres de los archivos
 
 
-async function uploadFiles(){
+async function uploadFiles() {
 
     for (let file of fileList) {
 
-        console.log(file);
-
         if (path.extname(file) !== ".ass") continue
 
-        const pathFile = path.join(uploads, file);
+        const fileName = file;
+        const namePart = path.parse(fileName).name.split("_", 2);
+        const pathFile = path.join(uploads, fileName);
+        const contentFile = await fs.readFile(pathFile, "utf-8");
 
-        const subFile = await fs.readFile(pathFile, "utf-8");
+        const objSub = {
+            series: namePart[0],
+            episode: namePart[1],
+            filename: fileName,
+            content: contentFile
+        }
 
-        console.log(subFile);
+        //console.log(objSub);
 
         break
     }
-
 
 }
 
